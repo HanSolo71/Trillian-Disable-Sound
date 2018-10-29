cb320ad2ae1ae661cba9062d81a82f336e807e0d
HanSolo71 <HanSolo552@gmail.com>
2018-10-29 12:45:19 -0400

    Add files via upload

---
+++ Disable Trillian Sound.ps1
@@ -0,0 +1,13 @@
+﻿#Path of Horizon View Persona Profiles
+$XMLPaths = (Get-ChildItem "C:\Profiles\*\AppData\Roaming\Trillian\" -Recurse -Force | Where-Object {$_.Name -contains 'Events.xml'} | Select FullName).FullName
+
+
+
+Foreach($XMLPath in $XMLPaths) {
+$XML = [xml](Get-Content $XMLPath)
+$node = $xml.events.prefs.setting | Where {$_.Name -eq 'Sounds'}
+$node.value = '0'
+$XML.Save($XMLPath)
+$XMLPath
+$node
+}
